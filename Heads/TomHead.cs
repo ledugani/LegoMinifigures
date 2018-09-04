4c0d69e9c97aac204e3ea24e18eb5b9254cfeea7
Tom Dugan <tdugan85@gmail.com>
2018-09-04 18:07:07 -0500

    started stuff

---
+++ Heads/TomHead.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Heads
+{
+    class TomHead : HeadBase
+    {
+        public override bool HasHair { get { return true; } }
+        public string HairColor { get { return "brown"; } }
+        public string Texture { get; set; }
+
+        public override void Talk()
+        {
+            Console.WriteLine("That's what she said.");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("*Eats some nachos*");
+        }
+    }
+
+    abstract class HeadBase
+    {
+        public abstract bool HasHair { get; }
+
+        public virtual void Talk()
+        {
+            Console.WriteLine("That's not what she said.");
+        }
+
+        public abstract void Eat();
+    }
+}
---
+++ Minifigure.cs
@@ -0,0 +1,20 @@
+﻿using LegoMinifigures.Heads;
+
+namespace LegoMinifigures
+{
+    class Minifigure
+    {
+        private readonly HeadBase _head;
+
+        public Minifigure(HeadBase head)
+        {
+            _head = head;
+        }
+
+        public void DoStuff()
+        {
+            _head.Talk();
+            _head.Eat();
+        }
+    }
+}
